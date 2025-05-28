d9ef0ab85a12b2716155c561e7cbbdac7dc776f0
presentpeak14 <eliot718@gmail.com>
2025-05-28 10:24:34 -0400

    Added LeafNode and tests cases for LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,7 +10,7 @@ class HTMLNode:
     
     def props_to_html(self):
         if self.props is None:
-            return
+            return ""
         new_props = []
         for key, value in self.props.items():
             # Now you have the key and the value for each property
@@ -19,3 +19,17 @@ class HTMLNode:
     
     def __repr__(self):
         return(f"{self.tag}, {self.value}, {self.children}, {self.props}")
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 def test_props_to_html_basic():
     node = HTMLNode(props={"href": "https://www.google.com"})
@@ -14,3 +14,18 @@ def test_props_to_html_no_list():
     node = HTMLNode()
     expected_output = None
     assert node.props_to_html() == expected_output
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")

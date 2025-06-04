fe7ab7a06c6cfd41251c91e2cfce0d3c477cbbd0
presentpeak14 <eliot718@gmail.com>
2025-06-03 20:04:55 -0400

    added text_to_textnodes function to inline_markdown.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,6 @@
 from htmlnode import LeafNode
 from enum import Enum
 
-
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
@@ -42,3 +41,5 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     raise ValueError(f"invalid text type: {text_node.text_type}")
+
+
